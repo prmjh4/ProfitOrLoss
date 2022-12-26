e19a3eacbccbe0c2536e4f75770c0535df308c54
Prem Jha <prmjh4@gmail.com>
2022-12-26 10:43:39 +0530

    logic corrected

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ function calculateProfitAndLoss(initial, quant, current){
     if(initial>current){
         var loss= (initial-current)*quant;
         var lossPert= (loss/initial)*100;
-        console.log('Loss is ${loss} and percetage is ${lossPert}');
+        console.log(`Loss is ${loss} and percetage is ${lossPert}`);
         // showOutput('Loss is ${loss} and perctage is ${lossPert}');
     }else if(initial<current){
         var profit= (current-initial)*quant;
         var profitPercentage= (profit/initial)*100;
-        console.log('Profit is ${profit} and percentage is ${profitPercentage}%');
+        console.log(`Profit is ${profit} and percentage is ${profitPercentage}%`);
         // showOutput('Profit is ${profit} and percentage profit is ${profitPercentage}%');
     }else{
         showOutput('No Pain No Gain');
